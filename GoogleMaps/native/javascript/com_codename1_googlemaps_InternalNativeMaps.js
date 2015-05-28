34cc87aef7efd615189df0b2eea6d92afa1f2281
Steve Hannah <steve@weblite.ca>
2015-05-28 15:32:39 -0700

    Implemented addMarker and removeMarkers.  Also added support for google maps key.

--- GoogleMaps/native/javascript/com_codename1_googlemaps_InternalNativeMaps.js
+++ GoogleMaps/native/javascript/com_codename1_googlemaps_InternalNativeMaps.js
@@ -26,6 +26,8 @@ var o = {};
 
     o.calcScreenPosition__double_double = function(param1, param2, callback) {
         this.lastPoint = this.map.getProjection().fromLatLngToPoint(new google.maps.LatLng(param1, param2));
+        console.log("Calculating screen position");
+        console.log(this.lastPoint);
         callback.complete();
     };
 
@@ -74,62 +76,119 @@ var o = {};
     };
 
     o.calcLatLongPosition__int_int = function(param1, param2, callback) {
-        this.lastPosition = this.map.getProjection().fromPointToLatLng(new google.maps.Point(param1, param2));
+        
+        // retrieve the lat lng for the far extremities of the (visible) map
+        var latLngBounds = this.map.getBounds();
+        var neBound = latLngBounds.getNorthEast();
+        var swBound = latLngBounds.getSouthWest();
+
+        // convert the bounds in pixels
+        var neBoundInPx = this.map.getProjection().fromLatLngToPoint(neBound);
+        var swBoundInPx = this.map.getProjection().fromLatLngToPoint(swBound);
+
+        // compute the percent of x and y coordinates related to the div containing the map; in my case the screen
+        var procX = param1/jQuery(this.el).width();
+        var procY = param2/jQuery(this.el).height();
+
+        // compute new coordinates in pixels for lat and lng;
+        // for lng : subtract from the right edge of the container the left edge, 
+        // multiply it by the percentage where the x coordinate was on the screen
+        // related to the container in which the map is placed and add back the left boundary
+        // you should now have the Lng coordinate in pixels
+        // do the same for lat
+        var newLngInPx = (neBoundInPx.x - swBoundInPx.x) * procX + swBoundInPx.x;
+        var newLatInPx = (swBoundInPx.y - neBoundInPx.y) * procY + neBoundInPx.y;
+
+        // convert from google point in lat lng and have fun :)
+        var newLatLng = this.map.getProjection().fromPointToLatLng(new google.maps.Point(newLngInPx, newLatInPx));
+        
+        //this.lastPosition = this.map.getProjection().fromPointToLatLng(new google.maps.Point(param1, param2));
+        this.lastPosition = newLatLng;
         callback.complete();
     };
 
     o.setShowMyLocation__boolean = function(param1, callback) {
-        callback.error(new Error("Not implemented yet"));
+        console.log("Show my location not implemented yet in Javascript port");
+        callback.complete();
     };
 
     o.createNativeMap__int = function(param1, callback) {
-        this.el = jQuery('<div id=\"cn1-googlemaps-canvas\"></div>').get(0);
-        this.mapId = param1;
-        
-        var mapOptions = {
-            zoom: 8,
-            center: new google.maps.LatLng(-34.397, 150.644)
-        };
-        this.map = new google.maps.Map(this.el, mapOptions);
-        
         var self = this;
-        var fireTapEventStatic = this.$GLOBAL$.com_codename1_googlemaps_MapContainer.fireTapEventStatic__int_int_int$async;
-        google.maps.event.addListener(this.map, 'click', function(evt) {
-            //Point p = mapInstance.getProjection().toScreenLocation(point);
-            //MapContainer.fireTapEventStatic(InternalNativeMapsImpl.this.mapId, p.x, p.y);
-            var p = self.map.getProjection().fromLatLngToPoint(evt.latLng);
-            fireTapEventStatic(self.mapId, p.x, p.y);
-            
-        });
+        //jQuery(document).ready(function() {
+        (function(){
+            self.el = jQuery('<div id=\"cn1-googlemaps-canvas\"></div>').get(0);
+            self.mapId = param1;
+
+            var mapOptions = {
+                zoom: 8,
+                center: new google.maps.LatLng(-34.397, 150.644)
+            };
+            self.map = new google.maps.Map(self.el, mapOptions);
+
+            //var self = this;
+            var fireTapEventStatic = self.$GLOBAL$.com_codename1_googlemaps_MapContainer.fireTapEventStatic__int_int_int$async;
+            google.maps.event.addListener(self.map, 'click', function(evt) {
+                //Point p = mapInstance.getProjection().toScreenLocation(point);
+                //MapContainer.fireTapEventStatic(InternalNativeMapsImpl.this.mapId, p.x, p.y);
+                var p = self.map.getProjection().fromLatLngToPoint(evt.latLng);
+                fireTapEventStatic(self.mapId, p.x, p.y);
+
+            });
+
+            var fireMapChangeEvent = self.$GLOBAL$.com_codename1_googlemaps_MapContainer.fireMapChangeEvent__int_int_double_double$async;
+            google.maps.event.addListener(self.map, 'bounds_changed', function() {
+                fireMapChangeEvent(self.mapId, self.map.getZoom(), self.map.getCenter().lat(), self.map.getCenter().lng());
+            });
+            google.maps.event.addListener(self.map, 'center_changed', function() {
+                //console.log("Center changed");
+                fireMapChangeEvent(self.mapId, self.map.getZoom(), self.map.getCenter().lat(), self.map.getCenter().lng());
+            });
+            google.maps.event.addListener(self.map, 'zoom_changed', function() {
+                fireMapChangeEvent(self.mapId, self.map.getZoom(), self.map.getCenter().lat(), self.map.getCenter().lng());
+            });
+            google.maps.event.addListener(self.map, 'tilt_changed', function() {
+                fireMapChangeEvent(self.mapId, self.map.getZoom(), self.map.getCenter().lat(), self.map.getCenter().lng());
+            });
+            google.maps.event.addListener(self.map, 'heading_changed', function() {
+                fireMapChangeEvent(self.mapId, self.map.getZoom(), self.map.getCenter().lat(), self.map.getCenter().lng());
+            });
+
+
+
+            callback.complete(self.el);
+        })();
         
-        var fireMapChangeEvent = this.$GLOBAL$.com_codename1_googlemaps_MapContainer.fireMapChangeEvent__int_int_double_double$async;
-        google.maps.event.addListener(this.map, 'bounds_changed', function() {
-            fireMapChangeEvent(self.mapId, self.map.getZoom(), self.map.getCenter().lat(), self.map.getCenter().lng());
-        });
-        google.maps.event.addListener(this.map, 'center_changed', function() {
-            fireMapChangeEvent(self.mapId, self.map.getZoom(), self.map.getCenter().lat(), self.map.getCenter().lng());
-        });
-        google.maps.event.addListener(this.map, 'zoom_changed', function() {
-            fireMapChangeEvent(self.mapId, self.map.getZoom(), self.map.getCenter().lat(), self.map.getCenter().lng());
-        });
-        google.maps.event.addListener(this.map, 'tilt_changed', function() {
-            fireMapChangeEvent(self.mapId, self.map.getZoom(), self.map.getCenter().lat(), self.map.getCenter().lng());
-        });
-        google.maps.event.addListener(this.map, 'heading_changed', function() {
-            fireMapChangeEvent(self.mapId, self.map.getZoom(), self.map.getCenter().lat(), self.map.getCenter().lng());
-        });
-        
-        
-        
-        callback.complete(this.el);
     };
 
-    o.addMarker__byte_1ARRAY_double_double_java_lang_String_java_lang_String_boolean = function(param1, param2, param3, param4, param5, param6, callback) {
-        callback.error(new Error("Not implemented yet"));
+    o.addMarker__byte_1ARRAY_double_double_java_lang_String_java_lang_String_boolean = function(param1, lat, lon, text, snippet, cb, callback) {
+        var self = this;
+        var uint8 = new Uint8Array(param1);
+        var url = 'data:image/png;base64,' + window.arrayBufferToBase64(uint8.buffer);
+        var markerOpts = {
+            icon : url,
+            map : this.map,
+            position : new google.maps.LatLng(lat, lon),
+            title : text
+        };
+        
+        var key = uniqueIdCounter++;
+        this.markerLookup = this.markerLookup || {};
+        
+        var marker = new google.maps.Marker(markerOpts);
+        
+        var fireMarkerEvent = this.$GLOBAL$.com_codename1_googlemaps_MapContainer.fireMarkerEvent__int_long$async;
+        google.maps.event.addListener(marker, 'click', function() {
+            fireMarkerEvent(self.mapId, key);
+        });
+        
+        this.markerLookup[key] = marker;
+        
+        callback.complete(key);
     };
 
     o.setRotateGestureEnabled__boolean = function(param1, callback) {
-        callback.error(new Error("Not implemented yet"));
+        console.log("setRotateGestureEnabled not implemented yet in Javascript");
+        callback.complete();
     };
 
     o.finishPath__long = function(param1, callback) {
@@ -199,7 +258,17 @@ var o = {};
     };
 
     o.removeAllMarkers_ = function(callback) {
-        callback.error(new Error("Not implemented yet"));
+        var toRemove = [];
+        var self = this;
+        for (var key in this.markerLookup) {
+            self.markerLookup[key].setMap(null);
+            toRemove.push(key);
+        }
+        for (var i=0; i<toRemove.length; i++) {
+            delete this.markerLookup[toRemove[i]];
+        }
+        
+        callback.complete();
     };
 
     o.isSupported_ = function(callback) {
