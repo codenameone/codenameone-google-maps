d471d827aa685aaaaa2b0eb48c8321c70e523917
Keylon Duran <godlive777@gmail.com>
2017-03-17 22:08:03 -0400

    ----------------------- GoogleMaptest ----------------------- 1-fix title and snippet it not showing in android and in simulator 2-fix icon no added in simulator

--- GoogleMapsTest/src/com/codename1/test/googlemaps/GoogleMapsTestApp.java
+++ GoogleMapsTest/src/com/codename1/test/googlemaps/GoogleMapsTestApp.java
@@ -21,7 +21,10 @@ import com.codename1.components.ToastBar;
 import com.codename1.googlemaps.MapContainer;
 import com.codename1.googlemaps.MapContainer.MapObject;
 import com.codename1.maps.Coord;
+import com.codename1.maps.MapListener;
+import com.codename1.maps.providers.OpenStreetMapProvider;
 import com.codename1.ui.Button;
+import com.codename1.ui.Component;
 import com.codename1.ui.Container;
 import com.codename1.ui.Display;
 import com.codename1.ui.EncodedImage;
@@ -46,7 +49,10 @@ public class GoogleMapsTestApp {
 
     private static final String HTML_API_KEY = "AIzaSyBWeRU02YUYPdwRuMFyTKIXUbHjq6e35Gw";
     private Form current;
-
+    MapObject me; 
+    MapObject markerAdded;
+    boolean tapDisabled = false;
+    //-----------------------------------------------------------------------------------------
     public void init(Object context) {
         try {
             Resources theme = Resources.openLayered("/theme");
@@ -57,105 +63,186 @@ public class GoogleMapsTestApp {
             e.printStackTrace();
         }
     }
-    MapObject sydney;
+    //-----------------------------------------------------------------------------------------
     public void start() {
+        
         if (current != null) {
             current.show();
             return;
         }
+        
         Form hi = new Form("Native Maps Test");
         hi.setLayout(new BorderLayout());
-        final MapContainer cnt = new MapContainer(HTML_API_KEY);
+        final MapContainer cnt = new MapContainer(new OpenStreetMapProvider());
+        //final MapContainer cnt = new MapContainer(HTML_API_KEY);
         
-        Button btnMoveCamera = new Button("Move Camera");
-        btnMoveCamera.addActionListener(e->{
-            cnt.setCameraPosition(new Coord(-33.867, 151.206));
-        });
         Style s = new Style();
         s.setFgColor(0xff0000);
         s.setBgTransparency(0);
         FontImage markerImg = FontImage.createMaterial(FontImage.MATERIAL_PLACE, s, 3);
         
+        
+        Button btnMoveCamera = new Button("Move Camera");
         Button btnAddMarker = new Button("Add Marker");
-        btnAddMarker.addActionListener(e->{
-           
-            cnt.setCameraPosition(new Coord(41.889, -87.622));
-            cnt.addMarker(EncodedImage.createFromImage(markerImg, false), cnt.getCameraPosition(), "Hi marker", "Optional long description", new ActionListener() {
-                public void actionPerformed(ActionEvent evt) {
-                    ToastBar.showMessage("You clicked the marker", FontImage.MATERIAL_PLACE);
-                }
-            });
-            
-        });
-        
         Button btnAddPath = new Button("Add Path");
-        btnAddPath.addActionListener(e->{
-            
-            cnt.addPath(
-                    cnt.getCameraPosition(),
-                    new Coord(-33.866, 151.195), // Sydney
-                    new Coord(-18.142, 178.431),  // Fiji
-                    new Coord(21.291, -157.821),  // Hawaii
-                    new Coord(37.423, -122.091)  // Mountain View
-            );
-        });
-        
         Button btnClearAll = new Button("Clear All");
-        btnClearAll.addActionListener(e->{
-            cnt.clearMapLayers();
-        });
-        
-        MapObject mo = cnt.addMarker(EncodedImage.createFromImage(markerImg, false), new Coord(-33.866, 151.195), "test", "test",e->{
-            System.out.println("Marker clicked");
-            cnt.removeMapObject(sydney);
-        });
-        sydney = mo;
-        System.out.println("MO is "+mo);
-        mo = cnt.addMarker(EncodedImage.createFromImage(markerImg, false), new Coord(-18.142, 178.431), "test", "test",e->{
-            System.out.println("Marker clicked");
-        });
-        System.out.println("MO is "+mo);
-        cnt.addTapListener(e->{
-            if (tapDisabled) {
-                return;
+        btnMoveCamera.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                //cnt.setCameraPosition(new Coord(-33.867, 151.206));
             }
-            tapDisabled = true;
-            TextField enterName = new TextField();
-            Container wrapper = BoxLayout.encloseY(new Label("Name:"), enterName);
-            InteractionDialog dlg = new InteractionDialog("Add Marker");
-            dlg.getContentPane().add(wrapper);
-            enterName.setDoneListener(e2->{
-                String txt = enterName.getText();
-                cnt.addMarker(EncodedImage.createFromImage(markerImg, false), cnt.getCoordAtPosition(e.getX(), e.getY()), enterName.getText(), "", e3->{
-                    ToastBar.showMessage("You clicked "+txt, FontImage.MATERIAL_PLACE);
+        });
+        btnAddMarker.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                
+                //cnt.setCameraPosition(new Coord(41.889, -87.622));
+               markerAdded = cnt.addMarker(EncodedImage.createFromImage(markerImg, false), cnt.getCameraPosition(), "Hi marker", "Optional long description", new ActionListener() {
+
+                    public void actionPerformed(ActionEvent evt) {
+                        ToastBar.showMessage("You clicked the marker", FontImage.MATERIAL_PLACE);
+                    }
                 });
-                dlg.dispose();
-                tapDisabled = false;
-            });
-            dlg.showPopupDialog(new Rectangle(e.getX(), e.getY(), 10, 10));
-            enterName.startEditingAsync();
+            }
+        });
+        btnAddPath.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                cnt.addPath(
+                        cnt.getCameraPosition(),
+                        new Coord(-33.866, 151.195), // Sydney
+                        new Coord(-18.142, 178.431),  // Fiji
+                        new Coord(21.291, -157.821),  // Hawaii
+                        new Coord(37.423, -122.091)  // Mountain View
+                );
+            }
+        });
+        btnClearAll.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                cnt.clearMapLayers();
+            }
         });
         
-        Container root = LayeredLayout.encloseIn(
-                BorderLayout.center(cnt),
-                BorderLayout.south(
-                        FlowLayout.encloseBottom(btnMoveCamera, btnAddMarker, btnAddPath, btnClearAll)
-                )
-        );
+        //cnt.setCameraPosition(new Coord(18.4823039439783, -69.91718675560759));
+        cnt.zoom(new Coord(18.4823039439783, -69.91718675560759), 15);
+//        me = cnt.addMarker(EncodedImage.createFromImage(markerImg, false), new Coord(-33.866, 151.195), "test", "test", new ActionListener() {
+//            @Override
+//            public void actionPerformed(ActionEvent e) {
+//                System.out.println("Marker clicked");
+//                cnt.removeMapObject(me);
+//            }
+//        });
+//        
+//        System.out.println("me:"+me);
+//
+//        agent = cnt.addMarker(EncodedImage.createFromImage(markerImg, false), new Coord(-18.142, 178.431), "test", "test", new ActionListener() {
+//            @Override
+//            public void actionPerformed(ActionEvent e) {
+//                System.out.println("Marker clicked");
+//            }
+//        });
+//
+//        System.out.println("agent:"+agent);
+        
+        
+        cnt.addTapListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+//                if (tapDisabled) {
+//                    return;
+//                }
+//                tapDisabled = true;
+                
+                
+                TextField enterName = new TextField();
+                Button  add = new Button("add");
+                
+                Container wrapper = BoxLayout.encloseY(new Label("Name:"), enterName, add);
+                InteractionDialog dlg = new InteractionDialog("Add Marker");
+                dlg.getContentPane().add(wrapper);
+                
+//                enterName.setDoneListener(new ActionListener() {
+//                    @Override
+//                    public void actionPerformed(ActionEvent e2) {
+//                        String txt = enterName.getText();
+//                       me = cnt.addMarker(EncodedImage.createFromImage(markerImg, false), cnt.getCoordAtPosition(e.getX(), e.getY()), enterName.getText(), "", new ActionListener() {
+//                            @Override
+//                            public void actionPerformed(ActionEvent e3) {
+//                                ToastBar.showMessage("You clicked "+txt, FontImage.MATERIAL_PLACE);
+//                            }
+//                        });
+//                        dlg.dispose();
+//                        tapDisabled = false;
+//                    }
+//                });
+                add.addActionListener(new ActionListener() {
+                    @Override
+                    public void actionPerformed(ActionEvent evt) {
+
+                        String txt = enterName.getText();
+                        //if (me!=null)
+                            //cnt.removeMapObject(me);
+                        me = cnt.addMarker(EncodedImage.createFromImage(markerImg, false), cnt.getCoordAtPosition(e.getX(), e.getY()), enterName.getText(), "", new ActionListener() {
+                            @Override
+                            public void actionPerformed(ActionEvent e3) {
+                                ToastBar.showMessage("You clicked " + txt, FontImage.MATERIAL_PLACE);
+                            }
+                        });
+                        dlg.dispose();
+                    }
+                });
+                
+                dlg.showPopupDialog(new Rectangle(e.getX(), e.getY(), 10, 10));
+                enterName.startEditingAsync();
+            }
+        });
+        
+        
+        cnt.addMapListener(new MapListener() {
+            @Override
+            public void mapPositionUpdated(Component source, int zoom, Coord center) {
+                
+                
+                double latitude = center.getLatitude();
+                double longitude = center.getLongitude();
+
+                System.out.println("latitude :"+latitude);
+                System.out.println("longitude:"+longitude);
+                System.out.println("zoom     :"+zoom);
+                
+//                if (me!=null){
+//                    cnt.removeMapObject(me);
+//                    System.out.println("removing markert->"+latitude+","+longitude);
+//                }
+                me = cnt.addMarker(EncodedImage.createFromImage(markerImg, false), new Coord(latitude, longitude), "Me", "", new ActionListener() {
+                    @Override
+                    public void actionPerformed(ActionEvent e3) {
+                        ToastBar.showMessage("You clicked ", FontImage.MATERIAL_PLACE);
+                    }
+                });
+                
+                System.out.println(" markert->me:"+me);
+                
+                //cnt.revalidate();
+
+            }
+        });
+        
+        Container root = LayeredLayout.encloseIn(BorderLayout.center(cnt),BorderLayout.south(FlowLayout.encloseBottom(btnAddMarker, btnAddPath, btnClearAll)));
         
         hi.add(BorderLayout.CENTER, root);
         hi.show();
         
     }
-    boolean tapDisabled = false;
-
+    //-----------------------------------------------------------------------------------------
     public void stop() {
         current = Display.getInstance().getCurrent();
     }
-
+    //-----------------------------------------------------------------------------------------
     public void destroy() {
     }
-
+    //-----------------------------------------------------------------------------------------
     
 
 }
