4893f0ae82eab924975bde92f0395ca54946f789
Steve Hannah <steve@weblite.ca>
2022-02-25 07:13:10 -0800

    Added support for new build hint editor in the simulator.  The build hint editor in the simulator will include a GoogleMaps tab with fields and instructions on setting up build hints that are required.

--- GoogleMaps/javase/src/main/java/com/codename1/googlemaps/InternalNativeMapsImpl.java
+++ GoogleMaps/javase/src/main/java/com/codename1/googlemaps/InternalNativeMapsImpl.java
@@ -17,6 +17,42 @@
 package com.codename1.googlemaps;
 
 public class InternalNativeMapsImpl implements com.codename1.googlemaps.InternalNativeMaps{
+
+    public InternalNativeMapsImpl() {
+        /*
+        javascript.googlemaps.key=YOUR_JAVASCRIPT_API_KEY
+        android.xapplication=<meta-data android:name="com.google.android.maps.v2.API_KEY" android:value="YOUR_ANDROID_API_KEY"/>
+        ios.afterFinishLaunching=[GMSServices provideAPIKey:@"YOUR_IOS_API_KEY"];
+        android.min_sdk_version=19
+         */
+
+        System.setProperty("codename1.arg.{{#googlemaps#javascript.googlemaps.key}}.label", "Javascript API Key");
+        System.setProperty("codename1.arg.{{#googlemaps#javascript.googlemaps.key}}.description", "Please enter your Javascript API key.");
+        System.setProperty("codename1.arg.{{#googlemaps#javascript.googlemaps.key}}.link", "https://developers.google.com/maps/documentation/javascript/get-api-key Get Key");
+
+        System.setProperty("codename1.arg.{{#googlemaps#android.xapplication}}.label", "android.xapplication");
+        System.setProperty("codename1.arg.{{#googlemaps#android.xapplication}}.description", "Your android.xapplication build hint must inject your Android API key");
+        System.setProperty("codename1.arg.{{#googlemaps#android.xapplication}}.hint", "<meta-data android:name=\"com.google.android.maps.v2.API_KEY\" android:value=\"YOUR_ANDROID_API_KEY\"/>");
+        System.setProperty("codename1.arg.{{#googlemaps#android.xapplication}}.link", "https://developers.google.com/maps/documentation/android-sdk/get-api-key Get Key");
+
+        System.setProperty("codename1.arg.{{#googlemaps#ios.afterFinishLaunching}}.label", "ios.afterFinishLaunching");
+        System.setProperty("codename1.arg.{{#googlemaps#ios.afterFinishLaunching}}.description", "Your ios.afterFinishLaunching hint must inject your IOS API Key");
+        System.setProperty("codename1.arg.{{#googlemaps#ios.afterFinishLaunching}}.hint", "[GMSServices provideAPIKey:@\"YOUR_IOS_API_KEY\"];");
+        System.setProperty("codename1.arg.{{#googlemaps#ios.afterFinishLaunching}}.link", "https://developers.google.com/maps/documentation/ios-sdk/get-api-key Get Key");
+
+        System.setProperty("codename1.arg.{{#googlemaps#android.min_sdk_version}}.label", "Android Minimum SDK Version");
+        System.setProperty("codename1.arg.{{#googlemaps#android.min_sdk_version}}.description", "Your Android Minimum SDK Version must be at least 19");
+        System.setProperty("codename1.arg.{{#googlemaps#android.min_sdk_version}}.hint", "19");
+
+        System.setProperty("codename1.arg.{{@googlemaps}}.label", "Google Maps");
+        System.setProperty("codename1.arg.{{@googlemaps}}.description", "The following build hints are required for the Google Maps cn1lib to operate correctly.");
+
+
+
+
+
+    }
+
     public void initialize() {
     }
 
